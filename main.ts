b969a9936b2592c1cffe56727c825e58805ca400
SigurdFranz <83399053+SigurdFranz@users.noreply.github.com>
2025-04-24 08:32:20 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,60 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1337) {
-        pins.servoWritePin(AnalogPin.P1, 180)
-        pins.digitalWritePin(DigitalPin.P13, 1)
-    } else if (receivedNumber != 1337) {
-        pins.digitalWritePin(DigitalPin.P14, 1)
+    if (receivedNumber == 3241) {
+        Lås_åpen()
+        Glys_på()
+    } else if (receivedNumber < 3241) {
+        Rlys_på()
+        basic.pause(2000)
+        Rlys_av()
+    } else if (receivedNumber > 3241) {
+        Rlys_på()
+        basic.pause(2000)
+        Rlys_av()
     }
 })
+function Glys_av () {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+}
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P1, 0)
+    Lås_åpen()
+    Glys_på()
+    Rlys_på()
+    basic.pause(5000)
+    Glys_av()
+    Rlys_av()
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "hei") {
+        Lås_låst()
+        Glys_på()
+        Rlys_på()
+        basic.pause(5000)
+        Glys_av()
+        Rlys_av()
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P1, 180)
+    Lås_låst()
+    Glys_av()
+    Rlys_av()
 })
+function Rlys_av () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+}
+function Lås_åpen () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+}
+function Lås_låst () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+}
+function Glys_på () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+}
+function Rlys_på () {
+    pins.digitalWritePin(DigitalPin.P14, 1)
+}
 basic.showIcon(IconNames.Heart)
 radio.setGroup(125)
-pins.digitalWritePin(DigitalPin.P13, 0)
-pins.digitalWritePin(DigitalPin.P14, 0)
+Glys_på()
+Rlys_på()
+basic.pause(2000)
